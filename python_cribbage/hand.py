59133c6484741eee86b812d4c991e5047bad8188
Dennis Harrop <dennisjohnharrop@gmail.com>
2022-07-07 23:19:13 +0100

    Fix validator on hand

--- python_cribbage/hand.py
+++ python_cribbage/hand.py
@@ -5,6 +5,6 @@ from python_cribbage import card_set
 class Hand(card_set.CardSet):
     @validator("cards")
     def number_of_cards(cls, v):
-        if len(v) > 4:
-            raise ValueError("Too many cards in hand. Must be <= 4.")
+        if len(v) != 4 and len(v) != 6:
+            raise ValueError("Too many cards in hand. Must be either 4 or 6.")
         return v
